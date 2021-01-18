41c1da05cefa751d5d9b87b4ef40433ac933788f
mochen <442535701@qq.com>
2021-01-18 12:33:20 +0800

    登录完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,127 @@
 <template>
-<!-- 只能有一个标签 -->
   <div>
-    login
+    <el-container>
+      <el-header>
+        <img src="../assets/logo2.png" style="height: 80%;margin-top: 10px">
+      </el-header>
+      <el-main>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="账号:" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码:" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
 
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+<!--   footer   -->
+      <el-footer v-if="footer_show"></el-footer>
+    </el-container>
   </div>
-
-
 </template>
-
 <script>
+/*footer*/
+import footer from "@/views/footer";
+import axios from "axios";
 export default {
-  name: "Login"
+  data() {
+    return {
+      ruleForm: {
+        username: '',
+        password: '',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'change' }
+        ]
+      },
+      /*footer*/
+      footer_show: true
+    };
+  },
+  /*footer*/
+  components:{
+    'el-footer':footer,
+  },
+  methods: {
+    submitForm(formName) {
+      const _this = this
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 提交逻辑
+          this.$axios.post('http://localhost:8880/login', this.ruleForm).then((res)=>{
+            const token = res.headers['authorization']
+            _this.$store.commit('SET_TOKEN', token)
+            _this.$store.commit('SET_USERINFO', res.data.data)
+            /*跳转*/
+            _this.$router.push("/blogs")
+            console.log(res.data.data)
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    /*footer*/
+    footer:function (bool){
+      this.footer_show = bool;
+    }
+
+  }
 }
 </script>
+<style>
+.el-header, .el-footer {
+  /*background-color: #B3C0D1;*/
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-<style scoped>
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
 
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+/*111111111111111111111111111111111111*/
+.demo-ruleForm{
+  max-width: 500px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/views/footer.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div id="common-footer">
+    footer
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
